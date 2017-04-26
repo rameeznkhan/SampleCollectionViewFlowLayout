538d020ed45ddb12cb945bd5e70f4f990a373259
rameez <rameeznkhan@gmail.com>
2017-04-26 15:26:25 +0530

    Initial Commit

---
+++ SampleCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleCollectionView
+//
+//  Created by Rameez khan  on 26/04/17.
+//  Copyright © 2017 Rameez khan . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
